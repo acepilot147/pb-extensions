74b16c2f5d43980acbc4c434283308e8d74581ad
acepilot147 <acepilot147147@gmail.com>
2026-06-01 01:06:25 -0700

    v1.9.4: Descramble bug fix

--- 0.8/ComixTo/source.js
+++ 0.8/ComixTo/source.js
@@ -1717,7 +1717,7 @@ var _Sources = (() => {
 
   // src/ComixTo/ComixTo.ts
   var ComixToInfo = {
-    version: "1.9.3",
+    version: "1.9.4",
     name: "ComixTo",
     icon: "icon.png",
     author: "acepilot147",
@@ -1752,7 +1752,7 @@ var _Sources = (() => {
           },
           interceptResponse: async (response) => {
             const reqUrl = response.request?.url ?? "";
-            if (!/\/si\//.test(reqUrl) || !response.rawData) return response;
+            if (!/\/sii?\//.test(reqUrl) || !response.rawData) return response;
             const params = readScrambleHeaders(response.headers);
             if (!params) return response;
             try {
