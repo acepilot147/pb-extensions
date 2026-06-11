9e1682a0808126c139ef6731507f4d6b8dcfcb61
acepilot147 <acepilot147147@gmail.com>
2026-06-11 10:50:52 -0700

    v1.9.7: website update

--- 0.8/ComixTo/source.js
+++ 0.8/ComixTo/source.js
@@ -1704,7 +1704,7 @@ var _Sources = (() => {
     return /\.(webp|png|jpe?g|avif)(\?|#|$)/i.test(url) || /wowpic\d*\.|\/s?i+\d*\//i.test(url);
   }
   var ComixToInfo = {
-    version: "1.9.6",
+    version: "1.9.7",
     name: "ComixTo",
     icon: "icon.png",
     author: "acepilot147",
