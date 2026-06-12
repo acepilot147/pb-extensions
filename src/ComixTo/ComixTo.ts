41715f4e4a05f29b2667d57060fb624d360cc1a4
acepilot147 <acepilot147147@gmail.com>
2026-06-12 11:26:35 -0700

    v1.9.10: refresh constants for comix bundle 4cb7eca97842 (now 5 rounds)

--- src/ComixTo/ComixTo.ts
+++ src/ComixTo/ComixTo.ts
@@ -68,7 +68,7 @@ function isImageRequestUrl(url: string): boolean {
 }
 
 export const ComixToInfo: SourceInfo = {
-  version: "1.9.9",
+  version: "1.9.10",
   name: "ComixTo",
   icon: "icon.png",
   author: "acepilot147",
