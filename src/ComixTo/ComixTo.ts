ff25ab229cf0953f3249eac8bb7548858324d0b9
acepilot147 <acepilot147147@gmail.com>
2026-06-11 18:51:46 -0700

    v1.9.8: refresh sbox-cbc constants for comix bundle 6ffb1349405a

--- src/ComixTo/ComixTo.ts
+++ src/ComixTo/ComixTo.ts
@@ -68,7 +68,7 @@ function isImageRequestUrl(url: string): boolean {
 }
 
 export const ComixToInfo: SourceInfo = {
-  version: "1.9.7",
+  version: "1.9.8",
   name: "ComixTo",
   icon: "icon.png",
   author: "acepilot147",
