013c6a45825eb056aa75d47b0f9632a54bb80aaf
acepilot147 <acepilot147147@gmail.com>
2026-06-14 21:27:35 -0700

    v1.9.16: refresh comix signing for bundle e4408d902149 (sbox-cbc 3-round rotation)

--- src/ComixTo/ComixTo.ts
+++ src/ComixTo/ComixTo.ts
@@ -69,7 +69,7 @@ function isImageRequestUrl(url: string): boolean {
 }
 
 export const ComixToInfo: SourceInfo = {
-  version: "1.9.15",
+  version: "1.9.16",
   name: "ComixTo",
   icon: "icon.png",
   author: "acepilot147",
